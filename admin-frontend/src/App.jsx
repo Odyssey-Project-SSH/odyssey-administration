eb19815ec0eb47b4270ad3f527ab2ce2a6737a77
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2024-05-25 01:43:07 +0200

    ODY-58: News Dashboard

--- admin-frontend/src/App.jsx
+++ admin-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Authentication from './components/authentication/Authentication';
 import Dashboard from './components/dashboard/Dashboard';
+import News from './components/news/News';
 
 const App = () => {
 	return (
@@ -11,6 +12,7 @@ const App = () => {
 				<Routes>
 					<Route exact path="/" element={ <Authentication /> } />
 					<Route path="/dashboard" element={ <Dashboard /> } />
+					<Route path="/news" element={ <News />} />
 				</Routes>
 			</div>
 		</Router>
